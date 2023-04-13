3f7656c33c00d285f0e9f004d6a8122e18beaf89
vishal garg <vg19951995@gamil.com>
2023-04-13 13:11:52 +0530

    icon updated

--- src/components/contactus/ContactUs.js
+++ src/components/contactus/ContactUs.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { FaPhone, FaEnvelope } from "react-icons/fa";
 import "./ContactUs.css";
 import ContactForm from "./ContactForm";
 
@@ -13,12 +14,22 @@ const ContactUs = () => {
         <div className="address-section">
           <div className="Conatact-datails">
             <div>
-              <p className="address-section-p">Phone</p>
+              <p className="address-section-p">
+                <span className="phone">
+                  <FaPhone size="15px" />
+                </span>
+                &nbsp;&nbsp;Phone
+              </p>
               <p>8336326875</p>
               <p>8336326875</p>
             </div>
             <div>
-              <p className="address-section-p">Email Us</p>
+              <p className="address-section-p">
+                <span className="mail">
+                  <FaEnvelope size="15px" />
+                </span>
+                &nbsp;&nbsp;Email Us
+              </p>
               <p>sales@gulshanlameepack.com</p>
             </div>
           </div>
